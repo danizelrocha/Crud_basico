0d3f4d635c64c3ceb66132cf5a79f9fbe633b64f
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-02-01 20:25:05 -0300

    Teste-2

---
+++ js/teste.js
@@ -0,0 +1,35 @@
+console.log('Ola mundo! - teste');
+console.log('#ConstruindoCRUDS');
+
+
+/* Base de dados*/
+const miniRedeSocial = {
+    usuarios: [
+        {
+            username: 'odanielrocha',
+        }
+    ],
+    posts: [
+        {
+            id: 1,
+            owner: 'odanielrocha',
+            content: 'Meu primeiro tweet'
+        }
+    ],
+};
+
+
+/* còdigo de Front End: web */
+const $meuForm = document.querySelector('form');
+console.log($meuForm);
+
+$meuForm.addEventListener('submit', function criaPostController(infosDoEvento) {
+    infosDoEvento.preventDefault();
+    console.log('estamos criando um post novo!')
+    const $campoCriaPost = document.querySelector('input[name="CampoCriaPost"]');
+    const $ListaDePosts = document.querySelector('.ListaDePosts');
+
+    console.log();
+    $ListaDePosts.insertAdjacentHTML('afterbegin',`<li>${$campoCriaPost.value}</li>`);
+    $campoCriaPost.value ='';
+});
