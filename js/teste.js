a5d2a866fb924186a5005ee805d0060f590a7bac
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-02-02 13:09:17 -0300

    Inicio delete

--- js/teste.js
+++ js/teste.js
@@ -1,7 +1,6 @@
 console.log('Ola mundo! - teste');
 console.log('#ConstruindoCRUDS');
 
-
 /* Base de dados*/
 const miniRedeSocial = {
     usuarios: [
@@ -16,6 +15,11 @@ const miniRedeSocial = {
             content: 'Meu primeiro tweet'
         }
     ],
+    readPosts() {
+        miniRedeSocial.posts.forEach(({owner, content})=>{
+        miniRedeSocial.criaPost({ owner: owner, content: content}, true); 
+        })
+    },
     criaPost(dados, htmlOnly = false) {
         if(!htmlOnly){
             /*Cria Post na Memória (Array / Objeto)*/
@@ -27,26 +31,31 @@ const miniRedeSocial = {
         }
         /*Cria Post no Html */
         const $ListaDePosts = document.querySelector('.ListaDePosts');
-        $ListaDePosts.insertAdjacentHTML('afterbegin',`<li>${dados.content}</li>`);
+        $ListaDePosts.insertAdjacentHTML('afterbegin',`
+            <li>
+              <button>Delete</button>
+              ${dados.content}
+            </li>
+        `);
     }
 };
-
 /* còdigo de Front End: web */
 const $meuForm = document.querySelector('form');
 console.log($meuForm);
 
 /*CRUD:[READ] */
-miniRedeSocial.posts.forEach(({owner, content})=>{
-    miniRedeSocial.criaPost({ owner: owner, content: content}, true); 
-});
+    miniRedeSocial.readPosts();
 
 /*CRUD: [CREATE] */
 $meuForm.addEventListener('submit', function criaPostController(infosDoEvento) {
     infosDoEvento.preventDefault();
     console.log('estamos criando um post novo!')
     const $campoCriaPost = document.querySelector('input[name="CampoCriaPost"]');
-
     miniRedeSocial.criaPost({ owner: 'odanielrocha', content: $campoCriaPost.value}); 
-
     $campoCriaPost.value ='';
-});
+})
+
+/* CRUD: DELETE*/
+document.querySelector('.ListaDePosts').addEventListener('click', function (infosDoEvento){
+      console.log('Houve um click', infosDoEvento.target);
+    })
